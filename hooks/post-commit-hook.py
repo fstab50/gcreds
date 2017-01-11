9c848050cf6b6899d72ff326a2cd1286e78de583
blakeca00 <blakeca00@gmail.com>
2017-01-11 07:19:37 -0600

    hooks/post-commit-hook.py updated

--- hooks/post-commit-hook.py
+++ hooks/post-commit-hook.py
@@ -2,15 +2,17 @@
 
 # post-commit hook to produce gcreds function library
 
-sourcefile = 'gcreds'
+# source and target files
+source = 'gcreds'
+target = '../cfcli/lib/gcreds.fcn'
 
 try:
-    with open(sourcefile) as f1:
-        with open('../cfcli/lib/gcreds.fcn','a') as f2:
+    with open(source) as f1:
+        with open(target,'a') as f2:
             lines = f1.readlines()
             for line in lines:
                 f2.write(line)
                 if 'MAIN' in line:
                     break
 except FileNotFoundError:
-    print("\n" + sourcefile + " not found for opening\n")
+    print("\nEither " + source + " not found to open or " + target + " file not accessible\n")
