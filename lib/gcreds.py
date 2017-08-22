0d470e33f8e6afffdc132cde0ef937929aa9247f
blakeca00 <blakeca00@gmail.com>
2017-08-22 13:10:44 -0500

    lib/gcreds.py: python lib start

---
+++ lib/gcreds.py
@@ -0,0 +1,62 @@
+"""
+gcreds Module
+
+Args:
+
+Returns:
+
+"""
+
+import os
+import json
+import argparse
+import boto3
+from botocore.exceptions import ClientError
+
+
+class gcreds():
+    """
+    Parse environment variables, validate characters, convert type(s). default
+    should be used to avoid conversion of an variable and retain string type
+
+    Example usage:
+
+    >>> from lambda_utils import read_env_variable
+    >>> os.environ['DBUGMODE'] = 'True'
+    >>> myvar = read_env_variable('DBUGMODE')
+    >>> type(myvar)
+    True
+
+    >>> from lambda_utils import read_env_variable
+    >>> os.environ['MYVAR'] = '1345'
+    >>> myvar = read_env_variable('MYVAR', 'default')
+    >>> type(myvar)
+    str
+    """
+    def __init__(self):
+        """ initalization
+        Args:
+            - checks deps, existence of creds, existence of Settings
+
+        Returns:
+
+        """
+
+    def help_menu(self):
+        """ displays gcreds help menu, options """
+
+    def get_session_token(self, iam_user, code):
+        """ generates session token for use in gen temp credentials """
+
+
+    def generate_credentials(self, iam_user, roles):
+
+
+    def calc_remaing_time(self):
+        """ remaining time left in session and credential lifetime """
+
+    def display_time(self):
+
+
+class colors():
+    """ formats """
